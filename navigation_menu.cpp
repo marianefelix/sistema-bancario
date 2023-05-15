93dcd16f8140950b450a71d6bad90fc775f7e862
Mariane Felix <mariane.felix.dev@gmail.com>
2023-05-14 21:30:28 -0300

    feat: create navigation menu class
    - add the showOptions, handleSelectedOption methods;
    - and add the functions signatures for create account, get balance,
    credit, debit and transfer.

---
+++ navigation_menu.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include "bank.cpp"
+
+using namespace std;
+
+class NavigationMenu {
+public:
+  void showOptions() {
+    cout << "---------- Menu ----------" << endl;
+    cout << "1 - Criar conta" << endl;
+    cout << "2 - Consultar saldo" << endl;
+    cout << "3 - Creditar" << endl;
+    cout << "4 - Debitar" << endl;
+    cout << "5 - Realizar transferência" << endl;
+    cout << "6 - Sair" << endl;
+    cout << "---------------------------" << endl;
+  }
+
+  void handleSelectedOption(Bank& bank) {
+    int selectedOption;
+    cin >> selectedOption;
+
+    switch(selectedOption) {
+      case 1:
+        handleCreateAccount();
+        break;
+      case 2:
+        handleGetBalance();
+        break;
+      case 3:
+        handleCredit();
+        break;
+      case 4:
+        handleDebit();
+        break;
+      case 5:
+        handleTransfer();
+        break;
+      default:
+        cout << "Opção inválida" << endl;
+        break;
+    }
+  }
+
+  void handleCreateAccount() {}
+
+  void handleGetBalance() {}
+
+  void handleDebit() {}
+  
+  void handleTransfer() {}
+}
