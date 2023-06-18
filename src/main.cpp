8f73d983b398b9e89a2616694081e202d7d23f54
Mariane Felix <marianefelix59@gmail.com>
2023-06-18 14:19:46 -0300

    feat: add the api settings

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,31 @@
 #include <iostream>
 #include "../headers/bank.h"
 #include "../headers/navigation_menu.h"
+#include "../headers/bank_api.h"
+
 using namespace std;
 
 int main() {
+    Address addr = new Address(Ipv4::any(), Port(8080));
+    BankAPI api = new BankAPI(addr);
+    
+    api.init();
+    api.start();
+
+    cout << "Server is running on port 8080..." << endl;
+    cin.ignore();
+
     NavigationMenu navigationMenu;
-
     Bank bank;
-
     int selectedOption = 0;
 
     do {
         navigationMenu.showOptions();
-        std::cin >> selectedOption;
+        cin >> selectedOption;
         navigationMenu.handleSelectedOption(bank, selectedOption);
     } while (selectedOption < 8);
+
+    api.shutdown();
 
     return 0;
 }
