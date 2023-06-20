29e8242ed5e00806376f147ac9e53ad635b57ccd
Leticia Deodato <leticiamanuelletd@gmail.com>
2023-06-20 18:01:11 -0300

    feat: create bank system tests

--- tests/test.cpp
+++ tests/test.cpp
@@ -1,17 +1,17 @@
 // #define CATCH_CONFIG_MAIN
 // #include "lib/catch.hpp"
-// // #include <cstdint>
+// // // #include <cstdint>
 
-// // uint32_t factorial( uint32_t number ) {
-// //     return number <= 1 ? number : factorial(number-1) * number;
-// // }
+// // // uint32_t factorial( uint32_t number ) {
+// // //     return number <= 1 ? number : factorial(number-1) * number;
+// // // }
 
-// // TEST_CASE( "Factorials are computed", "[factorial]" ) {
-// //     REQUIRE( factorial( 1) == 1 );
-// //     REQUIRE( factorial( 2) == 2 );
-// //     REQUIRE( factorial( 4) == 6 );
-// //     REQUIRE( factorial(10) == 3'628'800 );
-// // }
+// // // TEST_CASE( "Factorials are computed", "[factorial]" ) {
+// // //     REQUIRE( factorial( 1) == 1 );
+// // //     REQUIRE( factorial( 2) == 2 );
+// // //     REQUIRE( factorial( 4) == 6 );
+// // //     REQUIRE( factorial(10) == 3'628'800 );
+// // // }
 
 // int main() {
 //     // Seu código principal aqui, se necessário
